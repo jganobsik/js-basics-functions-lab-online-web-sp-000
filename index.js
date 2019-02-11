466fd27e439934bf88fc9b85501ce0e6eacf618e
Joe Ganobsik <jganobsik@gmail.com>
2019-02-11 00:29:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (distance) {
+  let answer
+  if (distance > 42) {
+    answer = distance - 42;
+  }
+  else {
+    answer = 42 - distance;
+  }
+}
+ 
